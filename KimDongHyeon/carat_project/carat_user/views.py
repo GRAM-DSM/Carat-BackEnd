94e028111e2b6de0dae2a0c2cce6b5b21286dcb7
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-08-11 09:42:37 +0900

    docs : update views.py, models.py in applications

--- KimDongHyeon/carat_project/carat_user/views.py
+++ KimDongHyeon/carat_project/carat_user/views.py
@@ -85,6 +85,23 @@ class TokenCheckView(View):
         return HttpResponse(status=403)
 
 
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
+            if Users.objects.filter(email=payload['email']).exists():
+                user = Users.objects.get(email=payload['email'])
+                request.user = user
+            else:
+                return JsonResponse({'message': 'INVALID_TOKEN'}, status=400)
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status=400)
+
+        return func(self, request, *args, **kwargs)
+    return wrapper
+
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
