a593392090fc6e667267f7925837278203f2a8fe
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-08-22 08:48:22 +0900

    docs : update carat_carat/views.py

--- KimDongHyeon/carat_project/carat_carat/views.py
+++ KimDongHyeon/carat_project/carat_carat/views.py
@@ -20,6 +20,7 @@ import time
 - 캐럿 : 걍 안해줌22
 """
 
+
 def login_decorator(func):
     """ 로그인했는지 여부를 인증하는 데코레이터 """
     def wrapper(self, request, *args, **kwargs):
@@ -125,12 +126,48 @@ class do_carat(View):
     @login_decorator
     def post(self, request, id):
         """ 캐럿 하기 """
-        pass
+        if id[0] == 'r':     # 캐럿할 대상이 리캐링일 경우
+            if Recarings.objects.filter(id=id).exists():
+                id = Recarings.objects.get(id=id).caring.id
+            else:
+                return JsonResponse({'message': '캐럿할 리캐링이 존재하지 않습니다!'}, status=404)
+        if Carings.objects.filter(id=id).exists():
+            print('캐럿대상 글:', id, ' 캐럿하는 사람:', request.user.email)
+            if CaratList.objects.filter(
+                carat_user_email=Users.objects.get(email=request.user.email),
+                caring=Carings.objects.get(id=id)
+            ).exists():
+                return JsonResponse({'message': '이미 이캐링에 캐럿하였습니다!'}, status=400)
+            CaratList(
+                carat_user_email=Users.objects.get(email=request.user.email),
+                caring=Carings.objects.get(id=id)
+            ).save()
+            return HttpResponse(status=200)
+        return JsonResponse({'message': '캐럿할 캐링이 존재하지 않습니다!'}, status=404)
 
     @login_decorator
     def delete(self, request, id):
         """ 캐럿 취소 """
-        pass
+        if id[0] == 'r':     # 캐럿할 대상이 리캐링일 경우
+            if Recarings.objects.filter(id=id).exists():
+                id = Recarings.objects.get(id=id).caring.id
+            else:
+                return JsonResponse({'message': '캐럿취소할 리캐링이 존재하지 않습니다!'}, status=404)
+        if Carings.objects.filter(id=id).exists():
+            print('캐럿취소대상 글:', id, ' 캐럿취소하는 사람:', request.user.email)
+            if CaratList.objects.filter(
+                carat_user_email=Users.objects.get(email=request.user.email),
+                caring=Carings.objects.get(id=id)
+            ).exists():
+                carat = CaratList.objects.filter(
+                    carat_user_email=Users.objects.get(email=request.user.email),
+                    caring=Carings.objects.get(id=id)
+                )
+                print('삭제할 캐럿:', carat)
+                carat.delete()
+                return HttpResponse(status=200)
+            return JsonResponse({'message': '이미 캐럿이 취소되어 있습니다!'}, status=400)
+        return JsonResponse({'message': '캐럿취소할 캐링이 존재하지 않습니다!'}, status=404)
 
 
 class read_carat_list(View):
