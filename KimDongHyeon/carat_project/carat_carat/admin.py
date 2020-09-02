487acef0267b819a1e0756051a8011e8d400b96c
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-09-03 01:12:47 +0900

    code : update admin.py

--- KimDongHyeon/carat_project/carat_carat/admin.py
+++ KimDongHyeon/carat_project/carat_carat/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# TODO 프로필 이미지, 커버 이미지 처리 (view, model 둘다 수정)
+# TODO 에러 response 제대로 정리
+# TODO 루트 링크 페이지 만들기
+# TODO 개발 다 끝났으면 디버그 모드 끄기
+
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['email', 'hashed_password', 'created_at']
+
+
+@admin.register(Carings)
+class CaringsAdmin(admin.ModelAdmin):
+    list_display = ['user_email', 'caring', 'image', 'created_at']
+
+
+@admin.register(CaratList)
+class CaratListAdmin(admin.ModelAdmin):
+    list_display = ['carat_user_email', 'caring']
+
+
+@admin.register(Profiles)
+class ProfilesAdmin(admin.ModelAdmin):
+    list_display = ['user_email', 'name', 'profile_image', 'cover_image', 'about_me']
+
+
+@admin.register(FollowList)
+class FollowListAdmin(admin.ModelAdmin):
+    list_display = ['follow_user_email', 'followed_user_email']
+
+
+@admin.register(Recarings)
+class RecaringsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_email', 'caring', 'created_at']
