f922aad0a5d202de13846f9e2f97b0cbc1b0f63a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-09-08 07:46:52 +0900

    code : edit view in carat_carat/caring

--- KimDongHyeon/carat_project/carat_carat/views/caring.py
+++ KimDongHyeon/carat_project/carat_carat/views/caring.py
@@ -5,20 +5,24 @@ class create_caring(View):
     @login_decorator
     def post(self, request):
         """ 캐링 생성하기 """
-        caring = Carings(
-            user_email=Users.objects.get(email=request.user.email),
-            caring=request.POST['caring'],
-            image='',
-            created_at=time.strftime('%Y-%m-%d %I:%M:%S', time.gmtime(timezone.now().timestamp())),
-        )
-        caring.save()
-        # 이미지 목록을 image 필드에 추가
-        for i, image in request.FILES.items():
-            image_url = file_upload('images/carings/', str(caring.id)+'-'+i[-1]+'.'+image.name.split('.')[-1], image)
-            caring.image += str(caring.id)+'-'+i[-1]+'.'+image.name.split('.')[-1] + ';'
-        caring.image = caring.image[:-1]
-        caring.save()
-        return JsonResponse({'created_caring_id': caring.id}, status=200)
+        try:
+            caring = Carings(
+                user_email=Users.objects.get(email=request.user.email),
+                caring=request.POST['caring'],
+                image='',
+                created_at=time.strftime('%Y-%m-%d %I:%M:%S', time.gmtime(timezone.now().timestamp())),
+            )
+            caring.save()
+            # 이미지들을 저장하고, 경로를 추출
+            for i, image in request.FILES.items():
+                image_url = file_upload('images/carings/', str(caring.id)+'-'+i[-1]+'.'+image.name.split('.')[-1], image)
+                caring.image += str(caring.id)+'-'+i[-1]+'.'+image.name.split('.')[-1] + ';'
+            if caring.image:    # 이미지 경로를 필드에 저장
+                caring.image = caring.image[:-1]
+                caring.save()
+            return JsonResponse({'created_caring_id': caring.id}, status=201)
+        except KeyError:
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
 
 class edit_caring(View):
@@ -40,13 +44,15 @@ class edit_caring(View):
                         image_url = file_upload('images/carings/',
                                                 str(target.id) + '-' + i[-1] + '.' + image.name.split('.')[-1], image)
                         target.image += str(target.id) + '-' + i[-1] + '.' + image.name.split('.')[-1] + ';'
-                    target.image = target.image[:-1]
-                    target.save()
-                    return HttpResponse(status=200)
-                return JsonResponse({'message': '수정할 권한이 없습니다! (내가 생성한 캐링이 아님)'}, status=403)
-            return JsonResponse({'message': '수정할 캐링이 존재하지 않습니다!'}, status=404)
+                    if target.image:
+                        target.image = target.image[:-1]
+                        target.save()
+                    return HttpResponse(status=201)
+                return JsonResponse({'message': "남이 생성한 캐링을 수정할 수 없습니다!(You can't edit caring created by others.)"},
+                                    status=403)
+            return JsonResponse({'message': '수정할 캐링이 존재하지 않습니다!(No caring exists to edit.)'}, status=404)
         except KeyError:
-            return JsonResponse({"message": "해당 캐링을 수정할 수 없습니다!"}, status=400)
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
     @login_decorator
     def delete(self, request, id):
@@ -61,11 +67,12 @@ class edit_caring(View):
                         if str(target.id) == file.split('-')[0]:
                             default_storage.delete('images/carings/' + file)
                     target.delete()
-                    return HttpResponse(status=200)
-                return JsonResponse({'message': '삭제할 권한이 없습니다! (내가 생성한 캐링이 아님)'}, status=403)
-            return JsonResponse({'message': '삭제할 캐링이 존재하지 않습니다!'}, status=404)
+                    return HttpResponse(status=204)
+                return JsonResponse({'message': "남이 생성한 캐링을 삭제할 수 없습니다!(You can't delete caring created by others.)"},
+                                    status=403)
+            return JsonResponse({'message': '삭제할 캐링이 존재하지 않습니다!(No caring exists to delete.)'}, status=404)
         except KeyError:
-            return JsonResponse({"message": "해당 캐링을 삭제할 수 없습니다!"}, status=400)
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
 
 class detail_caring(View):
