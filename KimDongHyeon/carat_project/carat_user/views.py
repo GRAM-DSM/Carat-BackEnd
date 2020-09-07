dc70b7367480def6d14e138cc46653877114bc97
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-09-07 20:08:14 +0900

    code : update views.py in carat_user

--- KimDongHyeon/carat_project/carat_user/views.py
+++ KimDongHyeon/carat_project/carat_user/views.py
@@ -22,11 +22,11 @@ def login_decorator(func):
             payload = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
             request.user = Users.objects.get(email=payload['email'])
         except jwt.exceptions.ExpiredSignatureError:
-            return JsonResponse({'message': '토큰의 서명이 만료되었습니다!'}, status=403)
+            return JsonResponse({'message': '토큰의 서명이 만료되었습니다!(Token expire.)'}, status=403)
         except jwt.exceptions.DecodeError:
-            return JsonResponse({'message': '존재하지 않는 토큰 값입니다!'}, status=401)
+            return JsonResponse({'message': '존재하지 않는 토큰 값입니다!(Token does not exist.)'}, status=401)
         except Users.DoesNotExist:
-            return JsonResponse({'message': '토큰의 사용자 값이 존재하지 않습니다!'}, status=404)
+            return JsonResponse({'message': '토큰의 사용자 값이 존재하지 않습니다!(Account for token does not exist.)'}, status=404)
         return func(self, request, *args, **kwargs)
     return wrapper
 
@@ -34,9 +34,6 @@ def login_decorator(func):
 class sign_up(View):
     def post(self, request):
         """ 계정 생성(회원 가입) """
-        print('이메일 :', request.POST['email'],
-              '\n비밀번호 :', request.POST['password'],
-              '\n생성시간 :', time.strftime('%Y-%m-%d %I:%M:%S', time.gmtime(timezone.now().timestamp())))
         try:
             if not Users.objects.filter(email=request.POST['email']).exists():  # 존재하는 이메일인지 확인
                 password = request.POST['password'].encode('utf-8')  # (비밀번호 암호화1) 입력된 패스워드를 바이트 형태로 인코딩
@@ -58,7 +55,7 @@ class sign_up(View):
                 return HttpResponse(status=201)
             return JsonResponse({"message": "이미 존재하는 이메일 입니다!(That email is already in use.)"}, status=409)
         except KeyError:
-            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request)"}, status=400)
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
     @login_decorator
     def delete(self, request):
@@ -66,13 +63,13 @@ class sign_up(View):
         try:
             if Users.objects.filter(email=request.user.email):
                 target = Users.objects.get(email=request.user.email)
-                print('탈퇴하는 유저:', target.email)
                 # 삭제할 프로필의 사진들을 미디어 폴더에서 삭제
                 for file in default_storage.listdir('images/profile/')[1]:
                     if target.email == file.split('-')[0]:
                         default_storage.delete('images/profile/' + file)
                 Users.objects.filter(email=request.user.email).delete()
                 return HttpResponse(status=200)
+            return JsonResponse({'maessage': '이미 존재하지 않는 유저 입니다!()'}, status=404)
         except KeyError:
             return JsonResponse({"message": "해당 유저를 탈퇴할 수 없습니다!"}, status=400)
 
@@ -96,13 +93,12 @@ class sign_in(View):
                                                 'exp': timezone.now().timestamp() + (86400 * 7),    # (86400 * 7) == 7일
                                                 'iss': 'dong'},     # 토큰 발행자 : dong(김동현)
                                                SECRET_KEY, algorithm="HS256").decode('utf-8')
-                    print('엑세스 토큰:', access_token, '\n리플레시 토큰:', refresh_token)
                     return JsonResponse({"access_token": access_token, "refresh_token": refresh_token}, status=200)
                 else:
                     return JsonResponse({"message": "비밀번호가 잘못되었습니다!(Email and password do not match.)"}, status=403)
             return JsonResponse({"message": "존재하지 않는 이메일입니다!(The account does not exist.)"}, status=400)
         except KeyError:
-            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request)"}, status=400)
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
     @login_decorator
     def get(self, request):
@@ -113,5 +109,4 @@ class sign_in(View):
                                    'exp': timezone.now().timestamp() + (3600 * 2),  # (3600 * 2) == 2시간
                                    'iss': 'dong'},  # 토큰 발행자 : dong(김동현)
                                   SECRET_KEY, algorithm="HS256").decode('utf-8')
-        print('새로 발급된 엑세스 토큰:', access_token)
         return JsonResponse({"access_token": access_token}, status=200)
