d499a67a2512496c277200c5a982b51cb5d492f0
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-09-08 08:28:08 +0900

    code : edit view in carat_carat/recaring

--- KimDongHyeon/carat_project/carat_carat/views/recaring.py
+++ KimDongHyeon/carat_project/carat_carat/views/recaring.py
@@ -5,22 +5,24 @@ class create_recaring(View):
     @login_decorator
     def post(self, request):
         """ 리캐링 생성하기 """
-        # 일단 리캐링의 id를 생성
-        recaring_id = ''
-        if Recarings.objects.all().exists():
-            recaring_id = f"r{int(Recarings.objects.order_by('-id')[0].id[1:])+1}"
-            print('recaring_id:', recaring_id)
-        # 그 후, 리캐링 생성
-        if Carings.objects.filter(id=request.POST.get('id')).exists():
-            recaring = Recarings(
-                id=recaring_id,
-                user_email=Users.objects.get(email=request.user.email),
-                caring=Carings.objects.get(id=request.POST.get('id')),
-                created_at=time.strftime('%Y-%m-%d %I:%M:%S', time.gmtime(timezone.now().timestamp()))
-            )
-            recaring.save()
-            return JsonResponse({'created_recaring_id': recaring.id}, status=200)
-        return JsonResponse({'message': '리캐링할 캐링이 존재하지 않습니다!'}, status=404)
+        try:
+            # 일단 리캐링의 id를 생성
+            recaring_id = ''
+            if Recarings.objects.all().exists():
+                recaring_id = f"r{int(Recarings.objects.order_by('-id')[0].id[1:])+1}"
+            # 그 후, 리캐링 생성
+            if Carings.objects.filter(id=request.POST.get('id')).exists():
+                recaring = Recarings(
+                    id=recaring_id,
+                    user_email=Users.objects.get(email=request.user.email),
+                    caring=Carings.objects.get(id=request.POST['id']),
+                    created_at=time.strftime('%Y-%m-%d %I:%M:%S', time.gmtime(timezone.now().timestamp()))
+                )
+                recaring.save()
+                return JsonResponse({'created_recaring_id': recaring.id}, status=201)
+            return JsonResponse({'message': '리캐링할 캐링이 존재하지 않습니다!(The caring to recaring does not exist.)'}, status=404)
+        except KeyError:
+            return JsonResponse({"message": "key 값이 잘못되었습니다!(a bad request.)"}, status=400)
 
 
 class delete_recaring(View):
@@ -30,11 +32,11 @@ class delete_recaring(View):
         if Recarings.objects.filter(id=id).exists():
             target = Recarings.objects.get(id=id)
             if target.user_email == Users.objects.get(email=request.user.email):
-                print('취소할 리캐링:', target)
                 target.delete()
-                return HttpResponse(status=200)
-            return JsonResponse({'message': '삭제할 권한이 없습니다! (내가 생성한 리캐링이 아님)'}, status=403)
-        return JsonResponse({'message': '삭제할 리캐링이 존재하지 않습니다!'}, status=404)
+                return HttpResponse(status=204)
+            return JsonResponse({'message': "남이 생성한 캐링을 삭제할 수 없습니다!(You can't delete recaring created by others.)"},
+                                status=403)
+        return JsonResponse({'message': '삭제할 리캐링이 존재하지 않습니다!(No recaring exists to delete.)'}, status=404)
 
 
 class read_recaring_list(View):
@@ -47,7 +49,6 @@ class read_recaring_list(View):
             else:
                 return JsonResponse({'message': '리캐링이 존재하지 않습니다!'}, status=404)
         if Carings.objects.filter(id=id).exists():
-            print('id :', id)
             li = []
             for recaring in Recarings.objects.filter(caring=Carings.objects.get(id=id)):
                 if li:  # 이미 리캐링한 사람이 리스트에 있으면 중복이므로 continue
